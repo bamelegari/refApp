5d586849e10c19ae5edd4f70b902fe4b74111f62
bamelegari <bamelegari@gmail.com>
2017-12-13 23:29:16 -0900

    made models.py, mostly from the mysql database I made before

--- refApp/refApp/settings.py
+++ refApp/refApp/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'refApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'referee',
+		'USER': 'root',
+		'PASSWORD': 'rootpass',
+		'HOST': 'localhost',
+		'PORT': '',
     }
 }
 
